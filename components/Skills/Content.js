a43758281d3d4e9a29937b5809311f4e33d6e47c
Pooja <pooja.thapa@dentsusegis.com>
2021-07-09 23:33:58 +0530

    feat: Add experience

---
+++ components/Skills/Content.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    text:
+      "Experience with JavaScript, HTML5, CSS3 and jQuery. Skilled in creating Single Page Applications (SPA) using ReactJS and implementing React routers",
+  },
+  {
+    text:
+      "Comfortable working with JSON data format to consume Restful web service",
+  },
+  {
+    text:
+      " Used Node.JS and React.JS MVC framework for developing the Single Page Application (SPA)"
+  },
+  {
+    text:
+      "Exposure to freelancing, experienced front-end developer with a working history in ReactJS, AngularJS, JavaScript, HTML, and CSS"
+  },
+  {
+    text:
+      "Experience in debugging and troubleshooting and familiar with debugging tools (Chrome debugger)"
+  },
+  {
+    text:
+      "Experience in using JIRA and following AGILE development process, and deep understanding in Software Development Life Cycle (SDLC) including requirements analysis, design, testing, implementation, and maintenance"
+  },
+  {
+    text: "Experience with versioning tools like GitHub"
+  },
+  {
+    text: "Excellent communication skills, with the ability to understand both technical and non-technical concepts and requirements, and efficient troubleshooting skills"
+  }
+];
+
+export default data;
--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -10,36 +10,52 @@ import JavaScript from "../../assets/skills/logo-javascript.svg";
 import Nodejs from "../../assets/skills/nodejs-icon.svg";
 import TailWind from "../../assets/skills/tailwind-css-2.svg";
 import NextJS from "../../assets/skills/next-js.svg";
+import Content from "./Content";
 
 export default function Skills() {
   return (
-      <>
-      <div className=" bg-gradient-to-r from-transparent to-pink-100 dark:skillsDarkGradient min-w-full absolute mt-24 sm:mt-36 h-full z-1 transform skew-y-12"></div>
-    <div className="p-10 relative lg:-mt-4 flex flex-col lg:flex-row sm:pr-20 lg:p-10 lg:items-center">
-      <div className="flex flex-col z-20 flexAdj mb-10 w-full lg:w-1/2 sm:ml-6 lg:ml-10 aos-init aos-animate">
-        <p className="text-left pb-5 text-blue-900 text-5xl font-LightName dark:text-gray-200 font-black">
-          SKILLS
-        </p>
-        <div className="flex flex-col border-none rounded-md w-full shadow-md border dark:border-gray-800 dark:shadow-2xl mt-10 p-5">
-          <div className="flex sm: h-60">
-            <div className="flex space-x-4 h-20 w-full flex-wrap justify-start">
-            <TechnicalSkills logo={HTML} name="HTML"/>
-            <TechnicalSkills logo={JavaScript} name="Javascript"/>
-            <TechnicalSkills logo={React} name="ReactJS"/>
-            <TechnicalSkills logo={Graphql} name ="Graphql"/>
-            <TechnicalSkills logo={Apollo} name ="Apollo"/>
-            <TechnicalSkills logo={TailWind} name="Tailwind"/>
-            <TechnicalSkills logo={Electron} name="Electron"/>
-            <TechnicalSkills logo={Angular} name="AngularJS"/>
-            <TechnicalSkills logo={Mongo} name="MongoDB"/>
-            <TechnicalSkills logo={Nodejs} name="NodeJS"/>
-            <TechnicalSkills logo={NextJS} name="NextJS"/>            
+    <div className="flex flex-col pb-10 relative">
+      <div className=" bg-gradient-to-r from-transparent to-pink-100 min-w-full absolute mt-24 sm:mt-36 h-full z-1 transform skew-y-12"></div>
+      <div className="p-10 lg:-mt-4 flex flex-col lg:flex-row sm:pr-20 lg:p-10 lg:items-center">
+        <div className="flex flex-col z-20 flexAdj mb-10 w-full lg:w-1/2 sm:ml-6 lg:ml-10">
+          <p className="text-left pb-5 text-blue-900 text-5xl font-medium dark:text-gray-200">
+            SKILLS
+          </p>
+          <div className="flex flex-col border-none rounded-md w-full shadow-md border dark:border-gray-800 dark:shadow-2xl mt-10 p-5">
+            <div className="flex sm: h-60">
+              <div className="flex space-x-4 h-20 w-full flex-wrap justify-start">
+                <TechnicalSkills logo={HTML} name="HTML" />
+                <TechnicalSkills logo={JavaScript} name="Javascript" />
+                <TechnicalSkills logo={React} name="ReactJS" />
+                <TechnicalSkills logo={Graphql} name="Graphql" />
+                <TechnicalSkills logo={Apollo} name="Apollo" />
+                <TechnicalSkills logo={TailWind} name="Tailwind" />
+                <TechnicalSkills logo={Electron} name="Electron" />
+                <TechnicalSkills logo={Angular} name="AngularJS" />
+                <TechnicalSkills logo={Mongo} name="MongoDB" />
+                <TechnicalSkills logo={Nodejs} name="NodeJS" />
+                <TechnicalSkills logo={NextJS} name="NextJS" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col z-20 w-full lg:w-1/2 sm:ml-6 lg:ml-10 abolute">
+          <div className="flex justify-center items-center flex-col sm:p-5 lg:p-0">
+            <div className="flex flex-col p-12 w-full md:w-3/4 lg:w-11/12 h-full bg-gray-50 shadow-1xl rounded-lg">
+              <p className="text-center pb-5 text-blue-900 text-5xl font-medium dark:text-gray-200">
+                Overall Experience
+              </p>
+              <div className="flex flex-col justify-start items-start text-sm">
+                <ul className="list-outside list-disc">
+                  {Content.map((content) => {
+                    return <li key="index" className="py-2 font-normal from-black">{content.text}</li>
+                  })}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div></div>
     </div>
-    </>
   );
 }
