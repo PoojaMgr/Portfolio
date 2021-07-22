4d3289023c5ddb97af9a5cd8eeb46d96f3094fdd
Pooja <pooja.thapa@dentsusegis.com>
2021-07-22 20:06:57 +0530

    restructure

--- frontend/components/Header/Header.js
+++ frontend/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header() {
         <header className="flex absolute w-full items-center justify-between p-4 pr-5 z-10">
          <Image  className="object-contain rounded-full" src={Face} alt="profilePic"/>
             <div className="flex flex-grow justify-evenly max-w-2xl">
-                <Link to="education" smooth duration={1000}><HeaderItem title="Education" Icon={AcademicCapIcon}/></Link>                
+                <Link to="education" smooth spy={true} activeClass={{backgroundColor: "black"}} duration={1000} ><HeaderItem title="Education" Icon={AcademicCapIcon}/></Link>                
                 <Link to="skills" smooth duration={1000}><HeaderItem title="Skills" Icon={LightBulbIcon}/></Link>
                 <Link to="projects" smooth duration={1000}><HeaderItem title="Projects" Icon={CodeIcon}/></Link>
                 <Link to="contact" smooth duration={1000}><HeaderItem title="Contact" Icon={InboxIcon}/></Link>
