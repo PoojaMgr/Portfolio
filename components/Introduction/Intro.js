f7b7369486465fc72ca15cac0a4ca31c5f25baa2
Pooja Thapa <poojathapa@Poojas-MacBook-Air.local>
2025-06-05 11:45:44 +0530

    version 2 update

--- components/Introduction/Intro.js
+++ components/Introduction/Intro.js
@@ -15,13 +15,13 @@ export default function Intro() {
     >
       <div className="self-center z-10 mt-6 relative">
         <h6 className="font-sans md:text-2xl sm:text-xl mt-10 text-lg mb-3 text-gray-700 pointer-events-none dark:text-gray-300 opacity-80">
-          Hey there, I'm
+          Hey there, I&#39;m
         </h6>
         <h1 className="dark:font-Codystar font-LightName md:text-6xl sm:text-6xl text-5xl pointer-events-none text-black-900 font-black">
           Pooja Thapa
         </h1>
         <p className="font-sans md:text-xl sm:text-lg text-md mt-3 text-gray-700 dark:text-gray-300 opacity-80">
-          Frontend Developer @{" "}
+          Frontend Developer &#64;{" "}
           <a
             href="https://infotechtion.com/"
             target="_blank"
